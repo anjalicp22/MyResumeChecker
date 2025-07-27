382e44d3f7f6b1c3b913cb75c0e5a9917d0f55bb
Anjali C <anjalicp22@gmail.com>
2025-07-28 00:38:41 +0530

    backend issue resolved

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // or from env
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000", 
   // headers: {
   //   'Content-Type': 'application/json',
   // },
